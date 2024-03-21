5bf20d346a36ae65f8fccf53beafc044bf4b931c
RadoKopacha <petyrnikolov0804@gmail.com>
2024-03-21 21:06:32 +0200

    qko

---
+++ Domashna21.3/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Domashna21._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter word: ");
+            string word = Console.ReadLine();
+            char[] duma = word.ToCharArray();
+            int mid = duma.Length / 2;
+            int i = 0;
+            int j = duma.Length - 1;
+            bool result = false;
+            if (duma.Length % 2 == 1)
+
+                for (i = 0; i < mid; i++)
+                {
+                    if (duma[i] == duma[duma.Length - 1 - i])
+                    {
+                        result = true;
+                    }
+                    else
+                    {
+                        result = false;
+                    }
+                }
+            if (result)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
